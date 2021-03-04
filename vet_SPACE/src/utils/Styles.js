4dd40b8d573c375895b31b2d559e9b09607f486d
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-03-04 16:56:08 +0900

    20210304
    디렉토리 정리

---
+++ vet_SPACE/src/utils/Styles.js
@@ -0,0 +1,18 @@
+export const colors = {
+    kuDarkGreen: 'rgb(3,107,63)',
+    kuLightGreen: 'rgb(184,243,41)',
+    kuGreen: 'rgb(33,186,49)',
+    kuMagenta: 'rgb(233,112,140)',
+    kuYellow: 'rgb(246,219,0)',
+    kuViolet: 'rgb(194,131,198)',
+    kuOrange: 'rgb(240,167,4)',
+    kuDarkGray: 'rgb(101,111,118)',
+    kuBlue: 'rgb(97,167,221)',
+    kuCoolGray: 'rgb(177,179,180)',
+    kuWarmGray: 'rgb(176,205,166)',
+    kuLightGray: 'rgb(236,235,227)',
+    kuBeige: 'rgb(240,244,218)',
+    kuBlack: 'rgb(0,0,0)',
+};
+
+// import { colors } from '../../utils/Styles';
