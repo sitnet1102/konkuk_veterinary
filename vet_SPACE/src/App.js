d46078cf80b16f85bba3efa1ced38500d954c807
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-03-05 17:09:08 +0900

    20210305
    App.js 필요없는 코드 정리

--- vet_SPACE/src/App.js
+++ vet_SPACE/src/App.js
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 import 'react-native-gesture-handler';
 import * as React from 'react';
 
