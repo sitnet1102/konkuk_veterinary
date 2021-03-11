5395a7739a64cb405f32b307e49f8786583fe402
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-03-11 17:55:12 +0900

    20210311
    강의실 예약 - 작성 완료
    완료
    
    로그인 페이지 - 자동 로그인 텍스트 클릭시 체크박스 연동 체크 되는 기능 추가

--- vet_SPACE/src/App.js
+++ vet_SPACE/src/App.js
@@ -54,8 +54,12 @@ function LoginScreen({navigation, route}) {
     2. 자동 로그인 옵션 확인
     3. 회원가입 연결 
     4. 네비게이션 옵션 연결 
+    5. 체크 박스랑 자동 로그인 텍스트 클릭이랑 연결해주기   -> 20210311
    */
   const [isSelected, setSelection] = React.useState(false);
+  const checkBoxChangeValue = React.useState(false);
+  const onPress = () => setSelection(()=>!isSelected);
+
   if (Platform.OS === 'web') {
     return (
       <View></View>
@@ -91,7 +95,9 @@ function LoginScreen({navigation, route}) {
                 boxType='square'
                 style={loginStyle.loginCheckBox}
               />
-              <Text style={loginStyle.loginAutoLoginText}>자동 로그인(Auto Login) / </Text>
+              <TouchableOpacity onPress={onPress}>
+                <Text style={loginStyle.loginAutoLoginText}>자동 로그인(Auto Login) / </Text>
+              </TouchableOpacity>
               <TouchableOpacity>
                 <Text style={loginStyle.loginNewAccButton}>회원가입(Sign Up)</Text>
               </TouchableOpacity>
@@ -277,7 +283,6 @@ function TimeSelectScreen(){
           <Text style={timeSelectStyle.timeSelectNextText}>다     음</Text>
         </TouchableOpacity>
       </View>
-
     </View>
   );
 } 
@@ -339,30 +344,53 @@ function DetailScreen(){
   );
 }
 function CompleteScreen(){
+  /**
+    추가 수정해야 하는 사항
+    1. 완료 네비게이션 연결
+    2. 홈버튼 연결하기
+    3. 메뉴 연결하기 
+  */
   return(
     <View style={completeStyle.container}>
       <View style={completeStyle.completeTop}>
         <Text style={completeStyle.completeTitleText}>예약신청 작성 완료</Text>
       </View>
       <View style={completeStyle.completeMid}>
-        <View style={completeStyle.completeContainer1}>
-          <Text style={completeStyle.completeText}>
-            아래 내용을 확인해주세요
+        <Text style={completeStyle.completeText}>
+          아래 내용을 확인해주세요
+        </Text>
+        <View style={completeStyle.completeLine}></View>
+        <Text style={completeStyle.completeText2}>
+          ▣ 이용시간 30분 전까지
+        </Text>
+        <Text style={completeStyle.completeText2}>
+          취소할 수 있습니다
+        </Text>
+        <Text style={completeStyle.completeText3}>
+          + 주말 예약일 경우 
+        </Text>
+        <View>
+          <Text style={completeStyle.completeText4}>
+            ▣ 대관신청서 출력 및 작성
           </Text>
-          <View style={completeStyle.completeLine}></View>
-          <Text style={completeStyle.completeText2}>
-              이용시간 30분 전까지
+          <Text style={completeStyle.completeText4}>
+            ▣ 주임교수님 확인 받기
           </Text>
-          <Text style={completeStyle.completeText2}>
-            취소할 수 있습니다
+          <Text style={completeStyle.completeText4}>
+            ▣ 행정실에 제출하여 
           </Text>
-        </View>
-        <View style={completeStyle.completeContainer2}>
-
+          <Text style={completeStyle.completeText5}>
+            총무 · 구매팀으로
+          </Text>
+          <Text style={completeStyle.completeText5}>
+            공문 요청하기 
+          </Text>
         </View>
       </View>
       <View style={completeStyle.completeBot}>
-
+        <TouchableOpacity style={completeStyle.completeNextButton}>
+          <Text style={completeStyle.completeNextText}>완  료</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -373,7 +401,7 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Complete">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen
           name="Start"
           component={StartScreen}
@@ -732,7 +760,7 @@ const timeSelectStyle = StyleSheet.create({
     flex: 1,
   },
   timeSelectMid: {
-    flex: 8,
+    flex: 12,
   },
   timeSelectBot: {
     flex: 2,
@@ -795,7 +823,7 @@ const timeSelectStyle = StyleSheet.create({
     fontWeight: 'bold',
   },
   timeSelectSheetDivision:  {
-    backgroundColor: colors.kuBeige,
+    backgroundColor: colors.kuLightGray,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
@@ -805,7 +833,7 @@ const timeSelectStyle = StyleSheet.create({
   },
   timeSelectScrollRow: {
     height: 40,
-    backgroundColor: colors.kuBeige,
+    backgroundColor: colors.kuLightGray,
   },
   timeSelectSheetText: {
     alignSelf: 'center',
@@ -914,12 +942,13 @@ const completeStyle=StyleSheet.create({
     flex: 1,
   },
   completeTop: {
-    flex: 3,
+    flex: 2,
     alignItems: 'center',
     justifyContent: 'center',
   },
   completeMid: {
-    flex: 7,
+    alignItems: 'center',
+    flex: 8,
   },
   completeBot: {
     flex: 2,
@@ -928,31 +957,56 @@ const completeStyle=StyleSheet.create({
     fontSize: 44,
     fontWeight: 'bold',
   },
-  completeContainer1: {
-    flex: 3,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  completeContainer2: {
-    flex: 4,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
   completeText: {
     fontSize: 32,
     fontWeight: 'bold',
   },
   completeText2: {
-    fontSize: 36,
+    fontSize: 28,
     fontWeight: 'bold',
+  },
+  completeText3: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    alignSelf: 'flex-start',
+    marginLeft: '10%',
+    marginBottom: '2%',
+    marginTop: '5%',
+  },
+  completeText4: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: '2%',
+  },
+  completeText5: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginLeft: '6%',
   },
   completeLine: {
     height: 1,
-    width: '80%',
+    width: '85%',
     backgroundColor: colors.kuBlack,
     margin: '2%',
   },
-
+  completeNextButton: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.kuDarkGreen,
+    width: '70%',
+    height: '60%',
+    opacity: 1,
+    borderWidth: 1,
+    borderRadius: 5,
+  }, 
+  completeNextText: {
+    alignSelf: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: colors.kuWhite,
+  },
+  
 });
 
 
