6de9052c71324791fbc7d330972ed37c9a205e54
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-03-18 16:04:20 +0900

    20210318
    drawer 재설치
    건대 로고 - 배경 없는 것 업데이트
    어플 아이콘 추가

--- vet_SPACE/src/utils/icons.js
+++ vet_SPACE/src/utils/icons.js
@@ -1,4 +1,4 @@
-export const IMG_KULOGO = require('../../assets/images/KULogo.jpg');
+export const IMG_KULOGO = require('../../assets/images/KULogo1.png');
 export const IMG_BACKGROUND = require('../../assets/images/background1.png');
 export const IC_SETTING = require('../../assets/icons/setting.png');
 export const IC_HOME = require('../../assets/icons/home1.png');
