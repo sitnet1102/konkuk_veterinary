1a34e3900439f6e9d8067a00fc9af2a1e2d3f53a
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-03-31 18:21:12 +0900

    20210331
    뒤로가기 아이콘 추가

--- vet_SPACE/src/utils/icons.js
+++ vet_SPACE/src/utils/icons.js
@@ -3,6 +3,7 @@ export const IMG_BACKGROUND = require('../../assets/images/background1.png');
 export const IC_SETTING = require('../../assets/icons/setting.png');
 export const IC_HOME = require('../../assets/icons/home1.png');
 export const IC_MENU = require('../../assets/icons/menu3.png');
+export const IC_BACK = require('../../assets/icons/back.png');
 /*
 export const IC_HOME1 = require('../../assets/icons/home1.png');
 export const IC_MENU1 = require('../../assets/icons/menu1.png');
