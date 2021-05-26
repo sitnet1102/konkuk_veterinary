f9de44d083a8750d2fd348081f44b9c693c22110
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-05-26 13:14:43 +0900

    20210526
    강의실 정보
    - 데이터 베이스 연결

--- vet_SPACE/src/components/screens/Screen06_01RoomInfoDetail.js
+++ vet_SPACE/src/components/screens/Screen06_01RoomInfoDetail.js
@@ -14,9 +14,9 @@ import LocationSelectModal from '../modal/LocationSelectModal';
 
 export default function RoomInfoDetailScreen({route}) {
   /**
-    1. 이미지 선택시 오류가 생길 수도 있음 -> 추후 수정 
-    2. 이미지 크기 고정이면 좋을것 같음 1024x768 -> 상관없음 
-    3. 강의실이 선택되지 않았을 때에 어떤식으로 할 것인지 고려해야함 
+    //1. 이미지 선택시 오류가 생길 수도 있음 -> 추후 수정 
+    //2. 이미지 크기 고정이면 좋을것 같음 1024x768 -> 상관없음 
+    //3. 강의실이 선택되지 않았을 때에 어떤식으로 할 것인지 고려해야함 
    */
   const [locationSelectModal, setLocationSelectModal] = React.useState(false);
   const [locationData, setLocationData] = React.useState('선 택');
@@ -26,6 +26,27 @@ export default function RoomInfoDetailScreen({route}) {
   const [images, setImages] = React.useState([
     "http://drive.google.com/uc?export=view&id=1fyr8wFS-UQNVT_tOCv_SoF7gDDYbdtue"
   ]);
+  const data1 = {
+    title: '호실정보',
+    d1: '* 분류: ',
+    d2: '* 수용인원: ',
+    d3: '* 전용면적: ',
+  };
+  const data2 = {
+    title: '기자재 현황',
+    d1: '* 빔프로젝터: ',
+    d2: '* 컴퓨터/전자교탁: ',
+    d3: '* 강의용 TV: ',
+    d4: '* 에어컨: ',
+  };
+  const [roomsort, setRoomsort] = React.useState(' ');
+  const [opacity, setOpacity] = React.useState(' ');
+  const [area, setArea] = React.useState(' ');
+
+  const [equip1, setEquip1] = React.useState(' ');
+  const [equip2, setEquip2] = React.useState(' ');
+  const [equip3, setEquip3] = React.useState(' ');
+  const [equip4, setEquip4] = React.useState(' ');
 
   const toggleLocationSelectModal = () => {
     setLocationSelectModal(prev => (!prev));
@@ -37,6 +58,7 @@ export default function RoomInfoDetailScreen({route}) {
     toggleLocationSelectModal();
     locationStyleChange();
     imageData(room);
+    detailData(building, room);
   };
   const locationStyleChange = () => {
     setLocationStyle(true);
@@ -50,64 +72,37 @@ export default function RoomInfoDetailScreen({route}) {
       ])
     }else{
       setImages(snapshot.val());
-      //console.log(snapshot.val());
     }
   })};
+
+  let detailData = (building, room) => {
+    database().ref('/Room_info/Detail/'+route.params.data+'/'+building+'/'+room).on('value', snapshot => {
+      console.log(snapshot.child('classification'));
+      if(snapshot.val() === null){
+        setArea(' ');
+        setRoomsort(' ');
+        setOpacity(' ');
+        setEquip1(' ');
+        setEquip2(' ');
+        setEquip3(' ');
+        setEquip4(' ');
+      }else{
+        setArea(snapshot.child('area').val());
+        setRoomsort(snapshot.child('classification').val());
+        setOpacity(snapshot.child('capacity').val());
+        setEquip1(snapshot.child('equip1').val());
+        setEquip2(snapshot.child('equip2').val());
+        setEquip3(snapshot.child('equip3').val());
+        setEquip4(snapshot.child('equip4').val());
+      }
+    })
+  };
 
   React.useEffect(() => {
     return () => database().ref().off('value', imageData);
   }, []);
 
-/**
-  <a href="https://ibb.co/Y7qC2wh">
-  <img src="https://i.ibb.co/7J6fvLK/Kakao-Talk-Photo-2021-04-01-16-40-29.jpg" 
-  alt="Kakao-Talk-Photo-2021-04-01-16-40-29" border="0"></a>
-  <a href="https://ibb.co/s1PhGYq">
-  <img src="https://i.ibb.co/GMJXLSR/Kakao-Talk-Photo-2021-04-01-16-40-42.jpg" 
-  alt="Kakao-Talk-Photo-2021-04-01-16-40-42" border="0"></a>
- */
-  /*
-  const images = [
-    //"https://source.unsplash.com/1024x768/?nature",
-    //"https://source.unsplash.com/1024x768/?tree",
-    //"https://source.unsplash.com/1024x768/?water",
-    //"https://source.unsplash.com/1024x768/?girl",
-    //"https://ifh.cc/g/vxZzuu.jpg",
-    //"https://ifh.cc/g/iL9x8G.jpg",
-    //"https://i.ibb.co/7J6fvLK/Kakao-Talk-Photo-2021-04-01-16-40-29.jpg",
-    //"https://i.ibb.co/GMJXLSR/Kakao-Talk-Photo-2021-04-01-16-40-42.jpg",
-    "https://firebasestorage.googleapis.com/v0/b/konkuk-vet-space.appspot.com/o/place01.jpeg?alt=media&token=5c4022a1-590a-4d8a-acda-c4b5a00f2a09",
-    "https://firebasestorage.googleapis.com/v0/b/konkuk-vet-space.appspot.com/o/place02.jpeg?alt=media&token=d2a772f1-4e9b-4865-86ef-bd0da5bd2484",
-    "https://firebasestorage.googleapis.com/v0/b/konkuk-vet-space.appspot.com/o/%E1%84%80%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%B4%E1%84%89%E1%85%B5%E1%86%AF%20207%E1%84%92%E1%85%A9%20(1).jpg?alt=media&token=3d8ce242-588c-4d31-8882-fc5af2f3c58d",
-    //"https://firebasestorage.googleapis.com/v0/b/konkuk-vet-space.appspot.com/o/%E1%84%80%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%B4%E1%84%89%E1%85%B5%E1%86%AF%20207%E1%84%92%E1%85%A9%20(1).jpg?alt=media&token=3d8ce242-588c-4d31-8882-fc5af2f3c58d",
-    "https://firebasestorage.googleapis.com/v0/b/konkuk-vet-space.appspot.com/o/%E1%84%80%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%B4%E1%84%89%E1%85%B5%E1%86%AF%20207%E1%84%92%E1%85%A9%20(2).jpg?alt=media&token=3382e933-177b-4c4c-822d-7b110ff385ef",
-    "https://firebasestorage.googleapis.com/v0/b/konkuk-vet-space.appspot.com/o/%E1%84%80%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%B4%E1%84%89%E1%85%B5%E1%86%AF%20207%E1%84%92%E1%85%A9%20(3).jpg?alt=media&token=53fcc659-274c-421c-bc8c-630007961424",
-    "https://firebasestorage.googleapis.com/v0/b/konkuk-vet-space.appspot.com/o/%E1%84%80%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%B4%E1%84%89%E1%85%B5%E1%86%AF%20207%E1%84%92%E1%85%A9%20(4).jpg?alt=media&token=9ab0b35c-d1f7-4737-b48e-ed856a045769",
-    "https://firebasestorage.googleapis.com/v0/b/konkuk-vet-space.appspot.com/o/dfsoeul_20160617_192932.jpeg?alt=media&token=bdbc893e-75fe-48c3-992f-25c5e44287f1",
-    "https://firebasestorage.googleapis.com/v0/b/konkuk-vet-space.appspot.com/o/%E1%84%80%E1%85%A5%E1%86%AB%E1%84%80%E1%85%AE%E1%86%A8%E1%84%83%E1%85%A2%E1%84%92%E1%85%A1%E1%86%A8%E1%84%80%E1%85%AD%20%E1%84%85%E1%85%A9%E1%84%80%E1%85%A9.png?alt=media&token=c376dfbf-5b5f-48db-866f-74e614c5a3b9",
-
-  ];
-  */
-  const data1 = {
-    title: ['호실정보'],
-    d1: ['* 분류: '],
-    d2: ['* 수용인원: '],
-    d3: ['* 전용면적: '],
-    d1_roomsort: ['강의실'],
-    d2_opacity: ['80명'],
-    d3_area: ['120m^2'],
-  };
-  const data2 = {
-    title: ['기자재 현황'],
-    d1: ['* 빔프로젝터: '],
-    d2: ['* 컴퓨터/전자교탁: '],
-    d3: ['* 강의용 TV: '],
-    d4: ['* 에어컨: '],
-    d1_: ['1대'],
-    d2_: ['1대'],
-    d3_: ['4대'],
-    d4_: ['천장형 2대'],
-  };
+  
 
   return(
     <View style={roominfodetailStyle.container}>
@@ -133,7 +128,6 @@ export default function RoomInfoDetailScreen({route}) {
           //ImageComponent={FastImage}
           images={images}
           sliderBoxHeight={RFPercentage(30)}
-          //onCurrentImagePressed={index => console.warn(`image ${index} pressed`)}
           dotColor={colors.kuLightGreen}
           inactiveDotColor={colors.kuCoolGray}
           paginationBoxVerticalPadding={20}
@@ -171,15 +165,15 @@ export default function RoomInfoDetailScreen({route}) {
             </Text>
             <View style={roominfodetailStyle.container3}>
               <Text style={roominfodetailStyle.text2}>{data1.d1}</Text>
-              <Text style={roominfodetailStyle.text2}>{data1.d1_roomsort}</Text>
+              <Text style={roominfodetailStyle.text2}>{roomsort}</Text>
             </View>
             <View style={roominfodetailStyle.container3}>
               <Text style={roominfodetailStyle.text2}>{data1.d2}</Text>
-              <Text style={roominfodetailStyle.text2}>{data1.d2_opacity}</Text>
+              <Text style={roominfodetailStyle.text2}>{opacity}</Text>
             </View>
             <View style={roominfodetailStyle.container3}>
               <Text style={roominfodetailStyle.text2}>{data1.d3}</Text>
-              <Text style={roominfodetailStyle.text2}>{data1.d3_area}</Text>
+              <Text style={roominfodetailStyle.text2}>{area}</Text>
             </View>
           </View>
           <View style={roominfodetailStyle.container2}>
@@ -188,19 +182,19 @@ export default function RoomInfoDetailScreen({route}) {
             </Text>
             <View style={roominfodetailStyle.container3}>
               <Text style={roominfodetailStyle.text2}>{data2.d1}</Text>
-              <Text style={roominfodetailStyle.text2}>{data2.d1_}</Text>
+              <Text style={roominfodetailStyle.text2}>{equip1}</Text>
             </View>
             <View style={roominfodetailStyle.container3}>
               <Text style={roominfodetailStyle.text2}>{data2.d2}</Text>
-              <Text style={roominfodetailStyle.text2}>{data2.d2_}</Text>
+              <Text style={roominfodetailStyle.text2}>{equip2}</Text>
             </View>
             <View style={roominfodetailStyle.container3}>
               <Text style={roominfodetailStyle.text2}>{data2.d3}</Text>
-              <Text style={roominfodetailStyle.text2}>{data2.d3_}</Text>
+              <Text style={roominfodetailStyle.text2}>{equip3}</Text>
             </View>
             <View style={roominfodetailStyle.container3}>
               <Text style={roominfodetailStyle.text2}>{data2.d4}</Text>
-              <Text style={roominfodetailStyle.text2}>{data2.d4_}</Text>
+              <Text style={roominfodetailStyle.text2}>{equip4}</Text>
             </View>
           </View>
         </View>
