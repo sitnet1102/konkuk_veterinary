f7a3a82a69a5dd37dd20af8470cfa999ddb729c1
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-06-01 20:07:12 +0900

    20210601
    firebase 경로 데이터

--- vet_SPACE/src/utils/firebaseData.js
+++ vet_SPACE/src/utils/firebaseData.js
@@ -1,2 +1,4 @@
 export const FIRESTORE_DATA1 = 'Booking_info_test';
 //export const FIRESTORE_DATA1 = 'Booking_info';
+export const FIRESTORE_DATA2 = 'User_info_test';
+//export const FIRESTORE_DATA2 = 'User_info';
