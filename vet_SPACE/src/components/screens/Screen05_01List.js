6d5e6f796edab80c034e8f7cefadd8a507152250
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-06-01 21:18:45 +0900

    20210601
    나의 예약 확인 리스트 - 예약이 없는 경우 처리

--- vet_SPACE/src/components/screens/Screen05_01List.js
+++ vet_SPACE/src/components/screens/Screen05_01List.js
@@ -26,8 +26,8 @@ export default function ListScreen({navigation}) {
     //2. 데이터 베이스에서 가져와서 시간 순서대로 보여주기  -> 삭제
     //3. 이전 예약 데이터는 하단에 다른 스타일로 넣어주기
     //4. 상단에 정렬 텝 연결 / 시간순, 강의실순 -> 삭제
-    5. 예약이 하나도 없을 경우에 UI 생각하기 -> 예약이 없습니다 
-    6. 이전 예약을 체크 하지 않았을 때에도 없습니다로 넣어주기   
+    //5. 예약이 하나도 없을 경우에 UI 생각하기 -> 예약이 없습니다 
+    //6. 이전 예약을 체크 하지 않았을 때에도 없습니다로 넣어주기   
   */
 
   const [isSelected, setSelection] = React.useState(false);
@@ -50,7 +50,12 @@ export default function ListScreen({navigation}) {
   let [tableData2, setTableData2] = React.useState([]);
 
   /// 임시 데이터 넣기
-
+  // 예약이 없는 경우 처리하기
+  const noReservation = () => {
+    Alert.alert('예약','예약이 없습니다.');
+    navigation.navigate('ReservCheck');
+  };
+  
   React.useEffect(() => {
     let tableData_tmp = [];
     let tableData_tmp2 = [];
@@ -58,8 +63,7 @@ export default function ListScreen({navigation}) {
     firestore().collection('User_info').doc(auth().currentUser.uid).collection('reservation').get()
     .then(querySnapshot => {
       if(querySnapshot.empty){
-        //Alert.alert('','예약이 없습니다.');
-        // 예약 없음의 경우가 많아서 일괄 처리하도록 함수로 만들 예정 
+        noReservation();
       }else{
         querySnapshot.forEach(doc => {
           firestore().collection(FIRESTORE_DATA1).doc(doc.get('date')).collection('Data').doc(doc.get('id')).get()
@@ -94,14 +98,14 @@ export default function ListScreen({navigation}) {
           });
         });
         // 예약 데이터가 없는 경우에 이전 페이지로 넘겨주기 
+        setTableData(tableData_tmp);
+        setTableData2(tableData_tmp2);
+        onPressRefresh();
       }
     }).catch(e => {
       Alert.alert('error510',e.code);
       navigation.navigate('ReservCheck');
     });
-    setTableData(tableData_tmp);
-    setTableData2(tableData_tmp2);
-    onPressRefresh();
   }, []);
 
   return(
@@ -156,46 +160,54 @@ export default function ListScreen({navigation}) {
             />
           }
         >
-          
-          <Table>
-            {
-              tableData.map((rowData, index) => (
-                <TableWrapper
-                  key={index}
-                  widthArr={state.widthArr}
-                  style={
-                    [listStyle.scrollRow] 
-                  }
-                  textStyle={listStyle.text2}
-                >
-                  <TouchableOpacity onPress={() => onPressRow(rowData)}>
-                    <View style={listStyle.tableTouchBox}>
-                      <Text style={listStyle.text3}>
-                        {
-                          rowData.detailData.room_id.split('/')[2] + ' ' + rowData.detailData.room_id.split('/')[3] + '\n\n' + rowData.date + '\n\n' + rowData.detailData.start_time + '~' + rowData.detailData.end_time
-                        }
-                      </Text>
-                      <View style={listStyle.textBox}>
-                        <Text 
-                          style={
-                            rowData.detailData.reserv_confirm ?
-                            listStyle.textConfirmed
-                            : listStyle.textUnconfirmed
-                          }
-                        >
+          {
+            tableData.length === 0 ?
+            <Text style={listStyle.text4}>
+              {'\n\n'}
+              예약이 없습니다
+              {'\n\n'}
+            </Text>
+            :
+            <Table>
+              {
+                tableData.map((rowData, index) => (
+                  <TableWrapper
+                    key={index}
+                    widthArr={state.widthArr}
+                    style={
+                      [listStyle.scrollRow] 
+                    }
+                    textStyle={listStyle.text2}
+                  >
+                    <TouchableOpacity onPress={() => onPressRow(rowData)}>
+                      <View style={listStyle.tableTouchBox}>
+                        <Text style={listStyle.text3}>
                           {
-                            rowData.detailData.reserv_confirm ?
-                            ' 예약 승인됨 '
-                            : ' 예약 대기중 '
+                            rowData.detailData.room_id.split('/')[2] + ' ' + rowData.detailData.room_id.split('/')[3] + '\n\n' + rowData.date + '\n\n' + rowData.detailData.start_time + '~' + rowData.detailData.end_time
                           }
                         </Text>
+                        <View style={listStyle.textBox}>
+                          <Text 
+                            style={
+                              rowData.detailData.reserv_confirm ?
+                              listStyle.textConfirmed
+                              : listStyle.textUnconfirmed
+                            }
+                          >
+                            {
+                              rowData.detailData.reserv_confirm ?
+                              ' 예약 승인됨 '
+                              : ' 예약 대기중 '
+                            }
+                          </Text>
+                        </View>
                       </View>
-                    </View>
-                  </TouchableOpacity>
-                </TableWrapper>
-              ))
-            }
-          </Table>
+                    </TouchableOpacity>
+                  </TableWrapper>
+                ))
+              }
+            </Table>
+          }
           {
             isSelected ? 
             <>
