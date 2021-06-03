3061f8858a22e7a72dd70cb778adc119a5be3c1e
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-06-03 13:48:41 +0900

    20210603
    firestore - 컬렉션 변경

--- vet_SPACE/src/utils/firebaseData.js
+++ vet_SPACE/src/utils/firebaseData.js
@@ -1,4 +1,4 @@
 export const FIRESTORE_DATA1 = 'Booking_info_test';
 //export const FIRESTORE_DATA1 = 'Booking_info';
-export const FIRESTORE_DATA2 = 'User_info_test';
-//export const FIRESTORE_DATA2 = 'User_info';
+//export const FIRESTORE_DATA2 = 'User_info_test';
+export const FIRESTORE_DATA2 = 'User_info';
