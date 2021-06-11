5503cd227bfa9dc86b94b93ac387d14e18082f9d
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-06-11 18:14:38 +0900

    20210611
    나의 예약 확인 리스트 - 화면 비율, 폰트 크기 조정

--- vet_SPACE/src/components/screens/Screen05_01List.js
+++ vet_SPACE/src/components/screens/Screen05_01List.js
@@ -3,33 +3,21 @@ import * as React from 'react';
 
 import {Text, View, StyleSheet, Alert, RefreshControl} from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
-import {Table, TableWrapper, Cell} from 'react-native-table-component';
-//import RadioButtonRN from 'radio-buttons-react-native';
+import {Table, TableWrapper} from 'react-native-table-component';
 import {TouchableOpacity, ScrollView} from 'react-native-gesture-handler';
 
 import {colors} from '../../utils/Styles';
 import {FIRESTORE_DATA1, FIRESTORE_DATA2} from '../../utils/firebaseData';
+import {horizontalScale, verticalScale, moderateScale} from '../../utils/scailing';
 
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
-
-import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 const wait = (timeout) => {
   return new Promise(resolve => setTimeout(resolve, timeout));
 };
 
 export default function ListScreen({navigation}) {
-  /**
-    수정할 사항
-    //1. 데이터 베이스 연결 
-    //2. 데이터 베이스에서 가져와서 시간 순서대로 보여주기  -> 삭제
-    //3. 이전 예약 데이터는 하단에 다른 스타일로 넣어주기
-    //4. 상단에 정렬 텝 연결 / 시간순, 강의실순 -> 삭제
-    //5. 예약이 하나도 없을 경우에 UI 생각하기 -> 예약이 없습니다 
-    //6. 이전 예약을 체크 하지 않았을 때에도 없습니다로 넣어주기   
-  */
-
   const [isSelected, setSelection] = React.useState(false);
   const onPressCheckBox = () => setSelection(()=>!isSelected);
   const [isRefresh, setRefresh] = React.useState(false);
@@ -38,18 +26,13 @@ export default function ListScreen({navigation}) {
     wait(2000).then(() => setRefresh(false));
   },[]);
   const onPressRow = (data) => navigation.navigate('ReservDetail', {data});
-  /*
-  const radiobuttondata = [
-    {label: '시간순'}, {label: '강의실순'}
-  ];
-  */
+  
   const state = {
     widthArr: ['100%'],
   };
   let [tableData, setTableData] = React.useState([]);
   let [tableData2, setTableData2] = React.useState([]);
 
-  /// 임시 데이터 넣기
   // 예약이 없는 경우 처리하기
   const noReservation = () => {
     Alert.alert('예약','예약이 없습니다.');
@@ -57,13 +40,8 @@ export default function ListScreen({navigation}) {
   };
   
   React.useEffect(() => {
-    //let tableData_tmp = [];
-    //let tableData_tmp2 = [];
-    
     firestore().collection(FIRESTORE_DATA2).doc(auth().currentUser.uid).collection('reservation').get()
     .then(querySnapshot => {
-      //setTableData([]);
-      //setTableData2([]);
       if(querySnapshot.empty){
         noReservation();
       }else{
@@ -99,9 +77,6 @@ export default function ListScreen({navigation}) {
             Alert.alert('error511',e.code);
           });
         });
-        // 예약 데이터가 없는 경우에 이전 페이지로 넘겨주기 
-        //setTableData(tableData_tmp);
-        //setTableData2(tableData_tmp2);
         onPressRefresh();
       }
     }).catch(e => {
@@ -128,32 +103,15 @@ export default function ListScreen({navigation}) {
             onTintColor={colors.kuDarkGreen}
           />
         </View>
-        <View style={listStyle.topContainer}>
+        <View>
           <TouchableOpacity onPress={onPressCheckBox}>
             <Text style={listStyle.text1}>이전 예약 보기  </Text>
           </TouchableOpacity>
         </View>
         <View style={listStyle.line}></View>
-        <View style={listStyle.topContainer}>
+        <View>
           <Text>아래로 드래그해서 새로고침하세요   </Text>
         </View>
-        {/*
-        <Text style={listStyle.text1}>  정렬 순서 :   </Text>
-        <View style={listStyle.sortRadio}>
-          <RadioButtonRN
-            style={listStyle.radiobutton}
-            data={radiobuttondata}
-            //selectedBtn={(e) => console.log(e)}
-            box={false}
-            initial={1}
-            boxStyle={listStyle.radiobuttonBox}
-            textStyle={listStyle.radiobuttonText}
-            activeColor={colors.kuDarkGreen}
-            circleSize={10}
-            labelHorizontal={false}
-          />
-        </View>
-        */}
       </View>
       <View style={listStyle.indexBox}>
         <View style={listStyle.unconfirmedColor}></View>
@@ -163,7 +121,6 @@ export default function ListScreen({navigation}) {
       </View>
       <View style={listStyle.bot}>
         <ScrollView 
-          style={listStyle.scroll}
           persistentScrollbar={true}
           refreshControl={
             <RefreshControl
@@ -269,27 +226,36 @@ const listStyle = StyleSheet.create({
     backgroundColor: colors.kuWhite,
   },
   top: {
-    height: RFPercentage(8),
+    //height: RFPercentage(8),
+    height: verticalScale(60),
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginVertical: 10,
+    marginVertical: verticalScale(10),
     marginHorizontal : '3%',
     borderWidth: 1,
     borderColor: colors.kuDarkGray,
     borderRadius: 5,
   },
+  checkBox: {
+    marginTop: verticalScale(4),
+    marginLeft: horizontalScale(5),
+    transform: [{scaleX: 0.8}, {scaleY: 0.8}]
+  },
+  text1: {
+    fontWeight: 'bold',
+    fontSize: moderateScale(16),
+  },
+  line: {
+    width: 1,
+    height: '60%',
+    backgroundColor: colors.kuDarkGray,
+    marginHorizontal: horizontalScale(10),
+  },
   indexBox: {
     flexDirection: 'row',
     justifyContent: 'flex-end',
     marginHorizontal : '3%',
-  },
-  confirmedColor: {
-    width: 15,
-    height: 15,
-    alignSelf: 'center',
-    marginHorizontal : '1%',
-    backgroundColor: colors.kuBlue,
   },
   unconfirmedColor: {
     width: 15,
@@ -297,6 +263,13 @@ const listStyle = StyleSheet.create({
     alignSelf: 'center',
     marginHorizontal : '1%',
     backgroundColor: colors.kuYellow,
+  },
+  confirmedColor: {
+    width: 15,
+    height: 15,
+    alignSelf: 'center',
+    marginHorizontal : '1%',
+    backgroundColor: colors.kuBlue,
   },
   indexText: {
     fontWeight: 'bold',
@@ -307,48 +280,16 @@ const listStyle = StyleSheet.create({
     marginBottom: '5%',
     marginTop: '2%',
   },
-  checkBox: {
-    marginTop: 4,
-    marginLeft: 5,
-    transform: [{scaleX: 0.8}, {scaleY: 0.8}]
-  },
-  text1: {
+  text4: {
     fontWeight: 'bold',
-    fontSize: 16,
-  },
-  line: {
-    width: 1,
-    height: '60%',
-    backgroundColor: colors.kuDarkGray,
-    marginHorizontal: 10,
-  },
-  topContainer: {
-    //flex: 1,
-    //marginLeft: 10,
-  },
-  sortRadio: {
-    flex: 1,
-    alignItems:'center',
-    marginBottom: 10,
-  },
-  radiobutton: {
-    flexDirection: 'row',
-    width: '51%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  radiobuttonbox: {
-    flex: 1,
-  },
-  radiobuttonText: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  scroll: {
-
+    fontSize: moderateScale(24),
+    lineHeight: verticalScale(28),
+    alignSelf: 'center',
+    marginTop: verticalScale(10),
+    marginBottom: verticalScale(30),
   },
   scrollRow: {
-    height: RFPercentage(25),
+    height: verticalScale(200),
     backgroundColor: colors.kuLightGray,
     borderWidth: 1,
     borderRadius: 5,
@@ -356,46 +297,32 @@ const listStyle = StyleSheet.create({
     marginVertical: 5,
   },
   text2: {
-    marginLeft: 20,
-    //alignSelf: 'center',
+    marginLeft: horizontalScale(20),
     fontWeight: 'bold',
-    fontSize: 20,
+    fontSize: moderateScale(20),
   },
   tableTouchBox: {
     flexDirection: 'row',
     width: '100%',
     height: '100%',
     //alignItems: 'center',
-    marginLeft: 20,
+    marginLeft: horizontalScale(20),
   },
   text3: {
     fontWeight: 'bold',
-    fontSize: RFValue(20),
-    lineHeight: RFValue(25),
+    fontSize: moderateScale(24),
+    lineHeight: verticalScale(30),
     alignSelf: 'center',
   },
-  line2: {
-    height: 1,
-    width: '100%',
-    backgroundColor: colors.kuDarkGray,
-    marginVertical: 10,
+  emptyBox: {
+    flex: 1,
   },
-  refresh: {
-    flexDirection: 'row',
-  },
-  text4: {
-    fontWeight: 'bold',
-    fontSize: RFValue(20),
-    lineHeight: RFValue(25),
-    alignSelf: 'center',
-    marginTop: 10,
-    marginBottom: 30,
-  },
-  text6: {
-    fontWeight: 'bold',
-    fontSize: RFValue(20),
-    lineHeight: RFValue(25),
-    marginTop: 10,
+  textBox: {
+    width: '14%',
+    height: '100%',
+    alignSelf: 'flex-end',
+    justifyContent: 'flex-end',
+    marginTop: verticalScale(20),
   },
   unconfirmedBox: {
     flex: 1,
@@ -405,14 +332,17 @@ const listStyle = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.kuYellow,
   },
-  textBox: {
-    width: '14%',
-    height: '100%',
-    alignSelf: 'flex-end',
-    justifyContent: 'flex-end',
-    marginTop: 20,
+
+  line2: {
+    height: 1,
+    width: '100%',
+    backgroundColor: colors.kuDarkGray,
+    marginVertical: verticalScale(10),
   },
-  emptyBox: {
-    flex: 1,
+  text6: {
+    fontWeight: 'bold',
+    fontSize: moderateScale(24),
+    lineHeight: verticalScale(30),
+    marginTop: verticalScale(10),
   },
 });
