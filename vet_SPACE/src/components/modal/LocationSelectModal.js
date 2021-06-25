233966d23394238c64ef4af34fe4164e43f5317c
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-06-25 14:09:36 +0900

    20210625
    장소 선택 모달 - 수의학관, 동생대 선택시 첫번째 선택 안되는 문제 -> 재수정

--- vet_SPACE/src/components/modal/LocationSelectModal.js
+++ vet_SPACE/src/components/modal/LocationSelectModal.js
@@ -28,18 +28,15 @@ export default function LocationSelectModal(props) {
 
   const buildingDataChanged = (itemValue) => {
     setSelectedBuilding(itemValue);
-    if(itemValue === "수의학관"){
-      setSelectedRoom('207');
-    }else{
-      setSelectedRoom('710-3');
-    }
     onRoomData = database()
     .ref('/Room_info/Room_info_list/'+props.classificationdata+'/'+itemValue)
     .on('value', snapshot => {
       if(!snapshot.val()){
         setRoomData(["없음"]);
+        setSelectedRoom("없음");
       }else{
         setRoomData(snapshot.val());
+        setSelectedRoom(snapshot.val()[0]);
       }
     });
   };
@@ -64,6 +61,7 @@ export default function LocationSelectModal(props) {
         "수의학관",
       ]);
     }
+    buildingDataChanged('수의학관');
   },[]);
 
   return(
